165f2b8629b26785c60303d5a9add6b58f4e5c40
apccmfamador <cmfamador@student.apc.edu.ph>
2025-03-06 22:30:07 +0800

    test

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['personal-website-finals-wine.vercel.app'],
+    unoptimized: true
+  },
   eslint: {
     ignoreDuringBuilds: true
   }
