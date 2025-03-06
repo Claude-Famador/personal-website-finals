9528c44a849a15de534fb0bdffbaa6a5b759cb8b
apccmfamador <cmfamador@student.apc.edu.ph>
2025-03-06 21:28:34 +0800

    next cfg fix

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  images: {
-    domains: ['fakeimg.pl'],
-    unoptimized: true
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(mp4|webm|gif|mp3|wav|woff|woff2|eot|ttf|otf)$/,
-      type: 'asset/resource',
-      generator: {
-        filename: 'static/media/[hash][ext][query]'
-      }
-    });
-    return config;
+  eslint: {
+    ignoreDuringBuilds: true
   }
 }
 
