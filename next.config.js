99a164b5753e8d3c868430799a57925606f8f532
apccmfamador <cmfamador@student.apc.edu.ph>
2025-03-06 21:07:17 +0800

    edited files

--- next.config.js
+++ next.config.js
@@ -2,6 +2,29 @@
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fakeimg.pl',
+      }
+    ]
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp4|webm)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/media/',
+          outputPath: 'static/media/',
+          name: '[name].[hash].[ext]',
+        },
+      },
+    });
+    return config;
   }
 }
 
